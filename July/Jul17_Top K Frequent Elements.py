ebf9c172bdd36437d7f5911c4ed32fba1e05dad6
Muhongfan <amber1216@live.cn>
2020-07-18 13:00:49 -0400

    Jul17

---
+++ July/Jul17_Top K Frequent Elements.py
@@ -0,0 +1,47 @@
+"""
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+You can return the answer in any order.
+"""
+
+from collections import Counter
+def topKFrequent(nums,k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: List[int]
+    """
+    #nums = set(nums)
+    result = Counter(nums)
+    k_freq = result.most_common(k)
+    k_freq = [k[0] for k in k_freq]
+    return k_freq
+
+print(topKFrequent([1,1,1,2,2,3],2))
+
+def topKFrequent2(nums,k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: List[int]
+    """
+    #nums = set(nums)
+    result = Counter(nums)
+    sorted_nums = sorted(result.items(), key=lambda x: x[1], reverse=True)
+    #print(sorted_nums[:k])
+    k_freq2 = [num[0] for num in sorted_nums[:k]]
+    return k_freq2
+print(topKFrequent2([1,1,1,2,2,3],2))
