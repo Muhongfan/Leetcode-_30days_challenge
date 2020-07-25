a58f460a21d018aeb77d1ac51eee4081f93b69fd
Muhongfan <amber1216@live.cn>
2020-07-24 22:35:26 -0400

    Jul24

--- July/Jul24_All Paths From Source to Target.py
+++ July/Jul24_All Paths From Source to Target.py
@@ -38,3 +38,21 @@ def dfs(graph, start, res, path):
 
 print(allPathsSourceTarget([[1,2], [3], [3], []] ))
 
+#demo Solu
+
+def allPathsSourceTargetdemo(graph):
+    """
+    :type graph: List[List[int]]
+    :rtype: List[List[int]]
+    """
+
+    def dfs(cur, path):
+        if cur == len(graph) - 1:
+            res.append(path)
+        else:
+            for i in graph[cur]:
+                dfs(i, path + [i])
+
+    res = []
+    dfs(0, [0])
+    return res
