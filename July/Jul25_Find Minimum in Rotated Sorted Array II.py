6bb1dd8ef936b09269064140ab377da6e4f48f6c
Muhongfan <amber1216@live.cn>
2020-07-26 22:23:29 -0400

    Jul26

--- July/Jul25_Find Minimum in Rotated Sorted Array II.py
+++ July/Jul25_Find Minimum in Rotated Sorted Array II.py
@@ -31,3 +31,20 @@ def findMin(nums):
     return nums[0]
 
 print(findMin([1,3,5]))
+
+#solu-less run time
+def findMin2(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    start, end = 0, len(nums) - 1
+    while start < end:
+        mid = start + (end - start) / 2
+        if nums[mid] > nums[end]:
+            start = mid + 1
+        elif nums[mid] < nums[end]:
+            end = mid
+        else:
+            end -= 1
+    return nums[start]
