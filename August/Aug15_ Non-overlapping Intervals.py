02d17d288766c3e4333bcdee4b7c146e2e423fa4
Muhongfan <amber1216@live.cn>
2020-08-19 22:52:58 -0400

    Aug19

--- August/Aug15_ Non-overlapping Intervals.py
+++ August/Aug15_ Non-overlapping Intervals.py
@@ -27,20 +27,21 @@ Intervals like [1,2] and [2,3] have borders "touching" but they don't overlap ea
 """
 
 
-def eraseOverlapIntervals(self, intervals):
+def eraseOverlapIntervals(intervals):
     """
-    :type intervals: List[Interval]
+    :type intervals: List[List[int]]
     :rtype: int
     """
-    if not intervals: return 0
-    intervals.sort(key=lambda x: x.start)
-    last = 0
-    res = 0
+    if not intervals:
+        return 0
+    intervals.sort(key=lambda x: x[0])  # 按左端点从小到大排序
+    temp_pos = 0
+    cnt = 0
     for i in range(1, len(intervals)):
-        if intervals[last].end > intervals[i].start:
-            if intervals[i].end < intervals[last].end:
-                last = i
-            res += 1
+        if intervals[temp_pos][1] > intervals[i][0]:  # 当当前区间右端点>i区间左端点
+            if intervals[i][1] <= intervals[temp_pos][1]:  # 当i区间右端点<当前区间右端点，表示i区间被覆盖在当前区间中
+                temp_pos = i  # 更新temp_pos，选择覆盖范围小的i区间
+            cnt += 1  # 当前区间右端点>i区间左端点都要计数+1
         else:
-            last = i
-    return res
+            temp_pos = i  # 当当前区间右端点<=i区间左端点，表示不重叠，要更新temp_pos
+print(eraseOverlapIntervals([[1,2],[2,3]]))
