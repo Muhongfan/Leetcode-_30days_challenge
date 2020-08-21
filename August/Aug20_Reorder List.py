0db6bfca06888f38216c4691dde85d903c75c95d
Muhongfan <amber1216@live.cn>
2020-08-20 23:58:35 -0400

    Aug20

---
+++ August/Aug20_Reorder List.py
@@ -0,0 +1,47 @@
+"""
+Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+Example 1:
+
+Given 1->2->3->4, reorder it to 1->4->2->3.
+Example 2:
+
+Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+"""
+def reorderList(self, head):
+    if head == None or head.next == None or head.next.next == None: return head
+
+    # break linked list into two equal length
+    slow = fast = head  # 快慢指针技巧
+    while fast and fast.next:  # 需要熟练掌握
+        slow = slow.next  # 链表操作中常用
+        fast = fast.next.next
+    head1 = head
+    head2 = slow.next
+    slow.next = None
+
+    # reverse linked list head2
+    dummy = ListNode(0);
+    dummy.next = head2  # 翻转前加一个头结点
+    p = head2.next;
+    head2.next = None  # 将p指向的节点一个一个插入到dummy后面
+    while p:  # 就完成了链表的翻转
+        tmp = p;
+        p = p.next  # 运行时注意去掉中文注释
+        tmp.next = dummy.next
+        dummy.next = tmp
+    head2 = dummy.next
+
+    # merge two linked list head1 and head2
+    p1 = head1;
+    p2 = head2
+    while p2:
+        tmp1 = p1.next;
+        tmp2 = p2.next
+        p1.next = p2;
+        p2.next = tmp1
+        p1 = tmp1;
+        p2 = tmp2
