d05253c3e90281d1eafd4566e8ff77b740f7d28c
Muhongfan <amber1216@live.cn>
2020-08-21 23:57:28 -0400

    Aug21

---
+++ August/Aug21_Sort Array By Parity.py
@@ -0,0 +1,31 @@
+"""
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+
+
+
+Example 1:
+
+Input: [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+
+Note:
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+"""
+def sortArrayByParity(self, A):
+    """
+    :type A: List[int]
+    :rtype: List[int]
+    """
+    l, r = 0, len(A) - 1
+    while l < r:
+        if A[l] & 1:
+            A[l], A[r] = A[r], A[l]
+        l += not (A[l] & 1)
+        r -= A[r] & 1
+    return A
