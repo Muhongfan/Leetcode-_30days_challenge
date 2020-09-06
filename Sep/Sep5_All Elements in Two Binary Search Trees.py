c7d4587cd11a374807e644682062bd720a5d8778
Muhongfan <amber1216@live.cn>
2020-09-05 23:57:49 -0400

    Sep5

--- Sep/Sep5_All Elements in Two Binary Search Trees.py
+++ Sep/Sep5_All Elements in Two Binary Search Trees.py
@@ -53,3 +53,30 @@ class Solution:
 
     def getAllElements(self, root1, root2):
         return sorted(self.inorder(root1) + self.inorder(root2))
+
+
+#demo
+    def getAllElements(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: List[int]
+        """
+        stack1, stack2, output = [], [], []
+        while root1 or root2 or stack1 or stack2:
+            while root1:
+                stack1.append(root1)
+                root1 = root1.left
+            while root2:
+                stack2.append(root2)
+                root2 = root2.left
+
+            if not stack2 or stack1 and stack1[-1].val <= stack2[-1].val:
+                root1 = stack1.pop()
+                output.append(root1.val)
+                root1 = root1.right
+            else:
+                root2 = stack2.pop()
+                output.append(root2.val)
+                root2 = root2.right
+        return output
