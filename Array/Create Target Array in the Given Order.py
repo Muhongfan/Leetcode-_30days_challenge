ef8aacbe7179bf2142bc336894f10c2bcadde774
Muhongfan <amber1216@live.cn>
2021-01-28 23:49:23 -0500

    1.29

--- Array/Create Target Array in the Given Order.py
+++ Array/Create Target Array in the Given Order.py
@@ -59,6 +59,15 @@ def createTargetArray(nums, index):
         answer.insert(index[i], nums[i])  # insert(位置, 值)
     return answer
 
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        target = []
 
-
+        for i in range(len(nums)):
+            target[:] = target[:index[i]] + [nums[i]] + target[index[i]:]
+        return target
 
