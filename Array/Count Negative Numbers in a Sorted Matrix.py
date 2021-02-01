0eaebb99b92b074f16d334772b8482c95de50836
Muhongfan <amber1216@live.cn>
2021-02-01 10:16:38 -0500

    1.31

--- Array/Count Negative Numbers in a Sorted Matrix.py
+++ Array/Count Negative Numbers in a Sorted Matrix.py
@@ -30,15 +30,61 @@ n == grid[i].length
 -100 <= grid[i][j] <= 100
 
 """
+import itertools
+
+
 class Solution(object):
+    # def countNegatives(self, grid):
+    #     """
+    #     :type grid: List[List[int]]
+    #     :rtype: int
+    #     """
+    #     count = 0
+    #     for item in grid:
+    #         for it in item:
+    #             if it < 0:
+    #                 count +=1
+    #     return count
+    def countNegatives(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        num = 0
+        for item in grid:
+            for i in range(len(item)):
+                if item[i] < 0:
+                    num += len(grid[0]) - i
+                    break
+        return num
+    #
+    # def countNegatives(self, grid):
+    #     """
+    #     :type grid: List[List[int]]
+    #     :rtype: int
+    #     """
+    #     flat = itertools.chain.from_iterable(grid)
+    #     count = 0
+    #     for elem in flat:
+    #         if elem < 0:
+    #             count += 1
+    #     return count
+
     def countNegatives(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
         count = 0
+        num = 0
         for item in grid:
-            for it in item:
-                if it < 0:
-                    count +=1
-        return count
+            for i in range(len(item)):
+                if item[i] < 0:
+                    num += len(grid[0]) - i
+                    break
+
+
+        return num
+
+so = Solution()
+print(so.countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]))
