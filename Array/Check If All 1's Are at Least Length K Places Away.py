b77472aca9143e780ee803bd554aa65b9829c0f0
Muhongfan <amber1216@live.cn>
2021-02-27 23:53:06 -0500

    Check If All 1's Are at Least Length K Places Away

--- Array/Check If All 1's Are at Least Length K Places Away.py
+++ Array/Check If All 1's Are at Least Length K Places Away.py
@@ -50,5 +50,21 @@ def kLengthApart(self, nums, k):
             left_1 = i
     return True
 
+def kLengthApart(self, nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: bool
+    """
+    prev = -k - 1
+    for i in xrange(len(nums)):
+        if not nums[i]:
+            continue
+        if i - prev <= k:
+            return False
+        prev = i
+    return True
+
+
 
 
