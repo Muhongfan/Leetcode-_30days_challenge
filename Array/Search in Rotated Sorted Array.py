bb87a736b49ac36a38952ca19424c9a6b4b7432a
Muhongfan <amber1216@live.cn>
2021-03-22 22:44:19 -0400

    .

--- Array/Search in Rotated Sorted Array.py
+++ Array/Search in Rotated Sorted Array.py
@@ -38,3 +38,36 @@ Explanation: [1,2] is the original sorted array.
 You can rotate the array by x = 5 positions to begin on the element of value 2: [2,1].
 
 """
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        length = len(nums)
+        left,right = 0,length-1
+
+        while left <= right:
+            mid = (left+right) // 2
+
+            # 如果mid刚好是所求，直接返回mid
+            if nums[mid] == target:
+                return mid
+
+            # 此时右半部分是有序的
+            elif nums[mid] < nums[right]:
+                if nums[mid] < target and nums[right] >= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            # 此时左半部分是有序的
+            else:
+                if nums[mid] > target and nums[left] <= target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+"""
+二分搜索法的关键在于获得了中间数后，判断下面要搜索左半段还是右半段，规律：如果中间的数小于最右边的数，则右半段是有序的，若中间数大于最右边数，则左半段是有序的，我们只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了。
+"""
