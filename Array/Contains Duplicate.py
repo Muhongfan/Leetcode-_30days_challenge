efdb55c3e3f1d6491194b3be56c5ff86eb9ad8bb
Muhongfan <amber1216@live.cn>
2021-03-24 23:52:49 -0400

    .

--- Array/Contains Duplicate.py
+++ Array/Contains Duplicate.py
@@ -25,9 +25,13 @@ Constraints:
 """
 
 
-def containsDuplicate(self, nums):
+def containsDuplicate(nums):
     nums.sort()
     for x in range(len(nums) - 1):
         if (nums[x] == nums[x + 1]):
             return True
     return False
+
+def containsDuplicate2(nums):
+    return len(set(nums)) != len(nums)
+
