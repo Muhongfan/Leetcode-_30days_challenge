c62e0bab1ea6604ffa86b1ce66da3a7d24196a35
Muhongfan <amber1216@live.cn>
2021-12-17 23:07:15 -0500

    BinaryTreeBuilt

---
+++ Tree/889. Construct Binary Tree from Preorder and Postorder Traversal.py
@@ -0,0 +1,35 @@
+"""
+Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a binary tree of distinct values and postorder is the postorder traversal of the same tree, reconstruct and return the binary tree.
+
+If there exist multiple answers, you can return any of them.
+
+
+
+Example 1:
+
+
+Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+Output: [1,2,3,4,5,6,7]
+Example 2:
+
+Input: preorder = [1], postorder = [1]
+Output: [1]
+
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        if len(preorder) == 1:
+            return root
+        i = postorder.index(preorder[1])
+        root.left = self.constructFromPrePost(preorder[1:i+2], postorder[:i+1])
+        root.right = self.constructFromPrePost(preorder[i+2:], postorder[i+1:-1])
+        return root
