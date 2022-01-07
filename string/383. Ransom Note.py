fd0a0ef20ec0753ec3fd06cd00ff6a3dd105a83b
Muhongfan <amber1216@live.cn>
2022-01-07 15:59:32 -0500

    string

---
+++ string/383. Ransom Note.py
@@ -0,0 +1,38 @@
+"""
+Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+"""
+# Strings are an immutable type.
+# This means that they can't be modified,
+# and so don't have "insert" and "delete" operations
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        for i in ransomNote:
+            if i not in magazine:
+                return False
+            location = magazine.index(i)
+            magazine = magazine[:location] + magazine[location+1:]
+        return True
