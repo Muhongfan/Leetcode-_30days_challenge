6b526833f0bf7da054a3ff7dce70add41aaed2da
Muhongfan <amber1216@live.cn>
2022-01-19 22:40:48 -0500

    linkedlist

---
+++ Algorithm/19. Remove Nth Node From End of List.py
@@ -0,0 +1,50 @@
+"""
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+
+        if head.next is None:
+            return None
+        fast, slow = head, head
+        for i in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        if n == 1:
+            slow.next = None
+        else:
+            slow.next = slow.next.next
+        return head
--- Algorithm/876. Middle of the Linked List.py
+++ Algorithm/876. Middle of the Linked List.py
@@ -19,3 +19,21 @@ Output: [4,5,6]
 Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
 
 """
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
