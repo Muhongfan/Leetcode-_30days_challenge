98da8f4610e35070d2ff20e74e649a917e3784c3
Muhongfan <amber1216@live.cn>
2022-04-09 22:28:21 -0400

    updated

---
+++ Algorithm/Recursion/119. Pascal's Triangle II.py
@@ -0,0 +1,50 @@
+"""
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+"""
+
+"""
+recursion
+decide the base cases
+"""
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        ans = [[1], [1, 1]]
+        if rowIndex == 0:
+            return ans[0]
+        elif rowIndex == 1:
+            return ans[1]
+        else:
+            for i in range(2, rowIndex + 1):
+                # row = [1 for _ in range(i + 1)]
+                ans.append([1] * (i + 1))
+                print(ans)
+                for j in range(1, i):
+                    ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j]
+                # ans.append(row)
+
+        return ans[-1]
+
+
+
+
+
+
+
